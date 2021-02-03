72e75e3e19eefb6a8f3367f381b70d9f41c44e9b
jgalvez <jgalvez@uliege.be>
2021-02-03 14:31:58 +0100

    wokrs ok with class

--- file-statistics/python-file-statistics_3.py
+++ file-statistics/python-file-statistics_3.py
@@ -15,14 +15,12 @@ def main():
 
     plot_num_lines(function_info)
 
-    print(general_counts)
-
+    general_counts.print()
 
 def print_lines_statistics_to_file(function_names, function_info):
     output_file = open("output.txt", 'w', encoding='utf-8')
     for i, name in enumerate(function_names):
-        output_file.write(
-            get_string_line_statistics(function_names[i], function_info[i]))
+        output_file.write(function_info[i].get_string_info(name[:-1]))
     output_file.close()
 
 def count_lines_of_each_type(line_type_list):
@@ -31,36 +29,20 @@ def count_lines_of_each_type(line_type_list):
         i += 1
 
     line_info = []
-    local_counts = create_counts()
-    general_counts = create_counts()
+    local_counts = CounterTypeLine()
+    general_counts = CounterTypeLine()
     for line_type in line_type_list[i:]:
         if line_type == "function":
             line_info.append(local_counts)
-            local_counts = create_counts()
-        increment_count(local_counts, line_type)
-        increment_count(general_counts, line_type)
+            local_counts = CounterTypeLine()
+        local_counts.increment_count(line_type)
+        general_counts.increment_count(line_type)
     return line_info, general_counts
-
-def increment_count(count, line_type):
-    if line_type == "blank":
-        count["blank"] += 1
-    elif line_type == "comment":
-        count["comment"] += 1
-    elif line_type == "code" or "function":
-        count["code"] += 1
-
-def create_counts():
-    count = {
-        "code": 0,
-        "blank": 0,
-        "comment": 0
-    }
-    return count
 
 def plot_num_lines(function_info):
     num_code_lines = []
     for counts in function_info:
-        num_code_lines.append(counts['code'])
+        num_code_lines.append(counts.get_count('code'))
     plt.plot(num_code_lines)
     plt.xlabel('ID method')
     plt.ylabel('# of lines / method')
@@ -111,11 +93,56 @@ def is_comment(line):
     return False
 
 def get_string_line_statistics(function_name, counts_line_type):
-    string_info = (f"{function_name[:-1]}: \n         "
-                   f"code: {counts_line_type['code']}, "
+    string_info = (f"{function_name}: \n         "
+                   f"Number of lines: code: {counts_line_type['code']}, "
                    f"comments:  {counts_line_type['comment']}, "
                    f"blanks: {counts_line_type['blank']} \n")
     return string_info
+
+class CounterTypeLine:
+    def __init__(self):
+        self.count = {
+        'code': 0,
+        'blank': 0,
+        'comment': 0
+    }
+
+    def increment_count(self, line_type):
+        if line_type == 'blank':
+            self.count['blank'] += 1
+        elif line_type == 'comment':
+            self.count['comment'] += 1
+        elif line_type == 'code' or 'function':
+            self.count['code'] += 1
+
+    def print(self):
+        self.print_line_contribution('blank')
+        self.print_line_contribution('comment')
+        self.print_line_contribution('code')
+        print ("Total lines: ", self.get_total_num_lines())
+
+    def print_line_contribution(self, line_type):
+        print (f"{line_type.capitalize()} lines: "
+               f"{self.get_count(line_type)},"
+               f" {self.get_line_contribution_percentage(line_type)}%")
+
+    def get_line_contribution_percentage(self, line_type):
+        return round(float(self.get_count(line_type) * 100) /
+               self.get_total_num_lines(), 2)
+
+    def get_count(self, name):
+        return self.count[name]
+
+    def get_string_info(self, name):
+        string_info = (f"{name}: \n         "
+                       f"code: {self.count['code']}, "
+                       f"comments:  {self.count['comment']}, "
+                       f"blanks: {self.count['blank']} \n")
+        return string_info
+
+    def get_total_num_lines(self):
+        return (self.count['blank'] + self.count['comment'] +
+                self.count['code'])
 
 if __name__ == "__main__":
     main()
