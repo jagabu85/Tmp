08290647b4dc4ae2eab6e5be0e6cd5431b92e2bd
jgalvez <jgalvez@uliege.be>
2021-02-03 14:36:36 +0100

    remove old files

--- file-statistics/python-file-statistics.py
+++ file-statistics/python-file-statistics.py
@@ -1,81 +1,152 @@
 import codecs
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import Counter
 
-def count_lines_of_each_type(file):
-    """ Cound the lines of a file by clasification (code, blank or comment).
-    :param file: text file
-    :type file: <class '_io.TextIOWrapper'>
+def main():
+    input_file = open("kernel.py", mode='r', encoding='utf-8')
+    function_names = get_function_names(input_file)
 
-    :return counts: Contain three integers with the keys: blank, comment and
-    ode.
-    :type counts: dictionary
-    """
-    counts = {
-        "code": 0,
-        "blank": 0,
-        "comment": 0
-    }
+    input_file.seek(0)
+    line_types = classify_lines(input_file)
+    function_info, general_counts = count_lines_of_each_type(line_types)
 
-    for line in file:
-        if line == "\n":
-            counts['blank'] += 1
-        elif line[0] == "#":
-            counts['comment'] += 1
-        else:
-            counts['code'] += 1
+    print_lines_statistics_to_file(function_names, function_info)
 
-    return counts
+    plot_num_lines(function_info)
 
-def print_line_counts(counts):
-    print_line_contribution('blank', counts)
-    print_line_contribution('comment', counts)
-    print_line_contribution('code', counts)
+    general_counts.print()
 
-    print ("Total lines: ",sum(counts.values()))
+def print_lines_statistics_to_file(function_names, function_info):
+    output_file = open("output.txt", 'w', encoding='utf-8')
+    for i, name in enumerate(function_names):
+        output_file.write(function_info[i].get_string_info(name[:-1]))
+    output_file.close()
 
-    return
+def count_lines_of_each_type(line_type_list):
+    i = 0
+    while line_type_list[i] != "function":
+        i += 1
 
-def print_line_contribution(line_type, line_info):
-    print (f"{line_type.capitalize()} lines: {line_info[line_type]},", \
-           f"{round(float(line_info[line_type] * 100) / sum(line_info.values()), 2)}%")
-    return
+    line_info = []
+    local_counts = CounterTypeLine()
+    general_counts = CounterTypeLine()
+    for line_type in line_type_list[i:]:
+        if line_type == "function":
+            line_info.append(local_counts)
+            local_counts = CounterTypeLine()
+        local_counts.increment_count(line_type)
+        general_counts.increment_count(line_type)
+    return line_info, general_counts
 
-def get_lines_functions_starts(file):
-    """ Get a list with the number of line where each function starts
-    :param file: text file
-    :type file: <class '_io.TextIOWrapper'>
-
-    :return function_starting_lines:List with the number of line where each
-    function starts
-    :type list
-    """
-
-    function_starting_lines =[]
-    for i, line in enumerate(file):
-        if line[0:3] == "def":
-            function_starting_lines.append(i)
-
-    return function_starting_lines
-
-def plot_num_lines(function_size):
-    plt.plot(function_size)
+def plot_num_lines(function_info):
+    num_code_lines = []
+    for counts in function_info:
+        num_code_lines.append(counts.get_count('code'))
+    plt.plot(num_code_lines)
     plt.xlabel('ID method')
     plt.ylabel('# of lines / method')
     plt.show()
 
-print("############################# Line types ")
-input_file_1 = open("kernel.py", mode='r', encoding='utf-8')
-line_counts = count_lines_of_each_type(input_file_1)
-print_line_counts(line_counts)
-input_file_1.close()
+def get_function_names(file):
+    function_names = []
+    for i, line in enumerate(file):
+        if line[0:3] == "def":
+            function_names.append(line[4:])
 
-print("############################# Chart number of lines ")
-input_file_2 = open("kernel.py", mode='r', encoding='utf-8')
-functions_starting_lines = np.array(get_lines_functions_starts(input_file_2))
-functions_size = functions_starting_lines[1:] - functions_starting_lines[0:-1]
-input_file_2.close()
-plot_num_lines(functions_size)
+    return function_names
+
+def classify_lines(file):
+    lines_type =[]
+    for i, line in enumerate(file):
+        if is_blank(line):
+            lines_type.append("blank")
+        elif is_function_def(line):
+            lines_type.append("function")
+        elif is_comment(line):
+            lines_type.append("comment")
+        else:
+            lines_type.append("code")
+
+    return lines_type
+
+def is_blank(line):
+    if line == "\n":
+        return True
+    else:
+        return False
+
+def is_function_def(line):
+    if line[0:3] == "def":
+        return True
+    else:
+        return False
+
+def is_comment(line):
+    for current_char in line:
+        if current_char == "#":
+            return True
+        elif current_char == '\t' or current_char == ' ':
+            None
+        else:
+            return False
+    return False
+
+def get_string_line_statistics(function_name, counts_line_type):
+    string_info = (f"{function_name}: \n         "
+                   f"Number of lines: code: {counts_line_type['code']}, "
+                   f"comments:  {counts_line_type['comment']}, "
+                   f"blanks: {counts_line_type['blank']} \n")
+    return string_info
+
+class CounterTypeLine:
+    def __init__(self):
+        self.count = {
+        'code': 0,
+        'blank': 0,
+        'comment': 0
+    }
+
+    def increment_count(self, line_type):
+        if line_type == 'blank':
+            self.count['blank'] += 1
+        elif line_type == 'comment':
+            self.count['comment'] += 1
+        elif line_type == 'code' or 'function':
+            self.count['code'] += 1
+
+    def print(self):
+        self.print_line_contribution('blank')
+        self.print_line_contribution('comment')
+        self.print_line_contribution('code')
+        print ("Total lines: ", self.get_total_num_lines())
+
+    def print_line_contribution(self, line_type):
+        print (f"{line_type.capitalize()} lines: "
+               f"{self.get_count(line_type)},"
+               f" {self.get_line_contribution_percentage(line_type)}%")
+
+    def get_line_contribution_percentage(self, line_type):
+        return round(float(self.get_count(line_type) * 100) /
+               self.get_total_num_lines(), 2)
+
+    def get_count(self, name):
+        return self.count[name]
+
+    def get_string_info(self, name):
+        string_info = (f"{name}: \n         "
+                       f"code: {self.count['code']}, "
+                       f"comments:  {self.count['comment']}, "
+                       f"blanks: {self.count['blank']} \n")
+        return string_info
+
+    def get_total_num_lines(self):
+        return (self.count['blank'] + self.count['comment'] +
+                self.count['code'])
+
+if __name__ == "__main__":
+    main()
+
 
 
 
